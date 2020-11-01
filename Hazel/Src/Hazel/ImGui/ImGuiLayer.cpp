cebe3a2d10ef2ded20e4c8a4a4aac6b832cb7d26
hwx0000 <hwx00001991@gmail.com>
2020-11-01 23:17:08 +0800

    Delete Debug Part

--- Hazel/Src/Hazel/ImGui/ImGuiLayer.cpp
+++ Hazel/Src/Hazel/ImGui/ImGuiLayer.cpp
@@ -16,38 +16,6 @@ Hazel::ImGuiLayer::~ImGuiLayer()
 
 void Hazel::ImGuiLayer::OnAttach()
 {
-	ImGui::CreateContext();
-	ImGui::StyleColorsDark();
-
-	ImGuiIO& io = ImGui::GetIO();
-	io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
-	io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
-
-	// TEMPORARY: should eventually use Hazel key codes
-	io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
-	io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
-	io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
-	io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
-	io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
-	io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
-	io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
-	io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
-	io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
-	io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
-	io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
-	io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
-	io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
-	io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
-	io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
-	io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
-	io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
-	io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
-	io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
-	io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
-	io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
-
-	ImGui_ImplOpenGL3_Init("#version 410");
-	/*
 	// 这里的函数，参考了ImGui给的例子：example_glfw_opengl3的文件里的main函数
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
@@ -83,7 +51,7 @@ void Hazel::ImGuiLayer::OnAttach()
 	io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
 
 	// ImGui为opengl做的初始设置
-	ImGui_ImplOpenGL3_Init("#version 410");*/
+	ImGui_ImplOpenGL3_Init("#version 410");
 }
 
 void Hazel::ImGuiLayer::OnDettach()
@@ -100,28 +68,10 @@ void Hazel::ImGuiLayer::OnEvent(Event &)
 
 void Hazel::ImGuiLayer::OnUpdate()
 {
-	ImGuiIO& io = ImGui::GetIO();
-	Application& app = Application::Get();
-	io.DisplaySize = ImVec2(app.GetWindow().GetWindowWidth(), app.GetWindow().GetWindowHeight());
-
-	float time = (float)glfwGetTime();
-	io.DeltaTime = m_Time > 0.0f ? (time - m_Time) : (1.0f / 60.0f);
-	m_Time = time;
-
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui::NewFrame();
-
-	static bool show = true;
-	ImGui::ShowDemoWindow(&show);
-
-	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-	/*
 	// 这里也是参照ImGui给的Example写Update的函数，主要做了以下功能
-	// 1. 创建Frame然后进行Render
-	// 2. 根据窗口大小，动态给ImGui设置窗口展示大小
-	// 3. 设置DeltaTime
+	// 创建Frame然后进行Render
+	// 根据窗口大小，动态给ImGui设置窗口展示大小
+	// 设置DeltaTime
 
 
 	ImGuiIO& io = ImGui::GetIO();
@@ -144,5 +94,5 @@ void Hazel::ImGuiLayer::OnUpdate()
 	ImGui::ShowDemoWindow(&showWindow);
 	// Rendering
 	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());*/
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
