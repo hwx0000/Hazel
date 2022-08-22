605f65e0996d9d8620a7937b9a12fd54c1272958
hwx0000 <wutong@papegames.net>
2022-08-22 15:28:39 +0800

    Fix bug when using rotating quads(know better about glm::decompose function)

--- Hazel/Src/Hazel/ECS/Components/Transform.cpp
+++ Hazel/Src/Hazel/ECS/Components/Transform.cpp
@@ -9,9 +9,11 @@ namespace Hazel
 	glm::mat4 Transform::GetTransformMat()
 	{
 		glm::mat4 translationM = glm::translate(Translation);
-		glm::mat4 rotationM = glm::rotate(glm::mat4(1.0f), Rotation.x, { 1, 0, 0 }) *
+
+		// Order: XYZ
+		glm::mat4 rotationM = glm::rotate(glm::mat4(1.0f), Rotation.z, { 0, 0, 1 }) *
 			glm::rotate(glm::mat4(1.0f), Rotation.y, { 0, 1, 0 }) *
-			glm::rotate(glm::mat4(1.0f), Rotation.z, { 0, 0, 1 });
+			glm::rotate(glm::mat4(1.0f), Rotation.x, { 1, 0, 0 });
 		glm::mat4 scaleM = glm::scale(glm::mat4(1.0f), Scale);
 
 		return translationM * rotationM * scaleM;
@@ -19,23 +21,14 @@ namespace Hazel
 
 	void Transform::SetTransformMat(const glm::mat4& trans)
 	{
-		//Rotation = { 1,0,0 };
-		//
-		//glm::quat QuatAroundX = glm::quat(Rotation.x, glm::vec3(1.0, 0.0, 0.0));
-		//glm::quat QuatAroundY = glm::quat(Rotation.y, glm::vec3(0.0, 1.0, 0.0));
-		//glm::quat QuatAroundZ = glm::quat(Rotation.z, glm::vec3(0.0, 0.0, 1.0));
-		//glm::quat q = QuatAroundX * QuatAroundY * QuatAroundZ;
-		//q = glm::normalize(q);
-		
-
-		// TODO: 旋转有问题
 		glm::quat q;
 		glm::vec3 skew;
 		glm::vec4 perspective;
 		glm::decompose(trans, Scale, q, Translation, skew, perspective);
-		//q = glm::conjugate(q);
-		// https://stackoverflow.com/questions/12048046/quaternions-euler-angles-rotation-matrix-trouble-glm
-		// looks like the function 'glm::eulerAngles' returns 'XYZ' as pitch, yaw, and roll.
-		Rotation = glm::eulerAngles(q);
+
+		auto qXYZ = glm::eulerAngles(q);// XYZ的顺序
+		Rotation.x = qXYZ.x;
+		Rotation.y = qXYZ.y;
+		Rotation.z = qXYZ.z;
 	}
 }
